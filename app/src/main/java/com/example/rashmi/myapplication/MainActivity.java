951e142d25156c3cb7b9698f9a87be2af6f0bcf4
priyabratam726 <priyabratam726@gmail.com>
2018-01-26 19:16:07 +0530

    2nd try

--- app/src/main/java/com/example/rashmi/myapplication/MainActivity.java
+++ app/src/main/java/com/example/rashmi/myapplication/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.rashmi.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView text1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text1 = (TextView) findViewById(R.id.text1);
+        //hello
     }
 }
